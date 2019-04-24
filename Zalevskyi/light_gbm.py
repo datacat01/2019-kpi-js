4e0ae5395ffa95710cb4a7105f1a7fd6067ff952
Vivikar <vzalevskyi24@gmail.com>
2019-04-25 01:38:04 +0300

    Updated lightgbm model

--- Zalevskyi/light_gbm.py
+++ Zalevskyi/light_gbm.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_spli
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 
 
@@ -25,9 +25,19 @@ obf_df = pd.read_csv(
 target = np.ones(len(obf_df))
 obf_df["target"] = target
 
+
+
 df = pd.concat([clear_df, obf_df], axis=0, ignore_index=True)
 X = df.drop(columns=["target"])
+
+#droping a string column (CHECK IF EXISTS)
+X = X.drop(columns=["file_name"])
+
 scl = StandardScaler()
+
+file_names = df.file_name.values
+indexes = df.index.values
+
 X = scl.fit_transform(X)
 y = df["target"]
 
@@ -39,7 +49,7 @@ def plot_confusion_matrix(y_true, y_pred, norm=True):
 
     cm = confusion_matrix(y_true, y_pred, labels=[0,1])
     if norm:
-        cm
+        cm = cm/len(y_pred)
     df_cm = pd.DataFrame(cm)
     ax = plt.axes()
     sns.heatmap(df_cm, annot=True,annot_kws={"size": 14}, fmt='g', cmap='Blues', ax=ax)
@@ -54,26 +64,26 @@ params['boosting_type'] = 'gbdt'
 params['objective'] = 'binary'
 params['metric'] = 'binary_logloss'
 params['sub_feature'] = 0.5
-params['num_leaves'] = 10
+params['num_leaves'] = 15
 params['min_data'] = 50
-params['max_depth'] = 10
+params['max_depth'] = 20
 
-clf = lgb.train(params, d_train, 100)
+clf = lgb.train(params, d_train, 700)
 
 #lightgbm evaluating
 y_pred_proba=clf.predict(x_test)
 y_pred = [int(y >= 0.5) for y in y_pred_proba]
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
-print("F1 dcore", f1_score(y_test, y_pred))
+print("F1 dcore:", f1_score(y_test, y_pred))
 print("Confusion Matrix")
 plot_confusion_matrix(y_test, y_pred)
+plot_confusion_matrix(y_test, y_pred, norm=False)
 
-cm = confusion_matrix(y_test, y_pred)
-
-print(cm)
-
-cm[0,:] = cm[0,:]/(len(y_pred) - sum(y_pred))
-cm[1,:]= cm[1,:]/(sum(y_pred))
-
-print(cm)
+#feature importances
+print("Feature Importances:")
+imprts = clf.feature_importance()
+ft_names = df.columns[:-2].values
+feature_importances = sorted(zip(imprts,ft_names), reverse=True)
+for imp in feature_importances:
+    print(imp)
